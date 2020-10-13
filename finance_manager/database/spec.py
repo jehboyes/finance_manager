d9aa94204b945358bed45571d4270ed56d3d8b4d
jehboyes <jehboyes@gmail.com>
2020-10-13 16:58:17 +0100

    fixed gaps in spec

--- finance_manager/database/spec.py
+++ finance_manager/database/spec.py
@@ -15,6 +15,7 @@ Note however that these are **not** used in the class names, for brevity. They a
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import Column, CHAR, VARCHAR, BOOLEAN, DECIMAL, DATE, INTEGER, create_engine, ForeignKey
+from finance_manager.functions import periods
 
 # Standard financial decimal
 FDec = DECIMAL(precision=18, scale=2)
@@ -67,7 +68,7 @@ class f_set(Base):
     set_id = Column(INTEGER(), primary_key=True)
     acad_year = Column(INTEGER(), nullable=False)
     costc = Column(CHAR(4), ForeignKey(
-        "cost_centre.costc", ondelete="CASCADE"), nullable=False)
+        "fs_cost_centre.costc", ondelete="CASCADE"), nullable=False)
     set_cat_id = Column(CHAR(3), ForeignKey(
         "f_set_cat.set_cat_id", ondelete="CASCADE"), nullable=False)
 
@@ -111,7 +112,7 @@ class finance(Base):
     f_set = relationship("f_set", back_populates="finances")
 
 
-class input_inc_courses(Base):
+class inc_courses(Base):
     __tablename__ = 'input_inc_courses'
     courses_id = Column(INTEGER(), primary_key=True, autoincrement=True,
                         mssql_identity_start=1000, mssql_identity_increment=1)
@@ -119,17 +120,12 @@ class input_inc_courses(Base):
     students = Column(INTEGER(), autoincrement=False, nullable=True)
     fee = Column(DECIMAL(precision=10, scale=5), nullable=True)
     set_id = Column(INTEGER(), ForeignKey("f_set.set_id"), nullable=False)
+    # Bad practice to use exec, but assigning to a dict wouldn't work
+    for n in periods():
+        exec(f"p{n} = Column(FDec)")
 
 
-class input_inc_courses_p(Base):
-    __tablename__ = "input_inc_courses_p"
-    courses_id = Column(INTEGER(), ForeignKey(
-        "input_inc_courses.courses_id"), primary_key=True, nullable=False)
-    period = Column(INTEGER(), primary_key=True, nullable=False)
-    amount = Column(FDec, nullable=True)
-
-
-class input_inc_other(Base):
+class inc_other(Base):
     __tablename__ = 'input_inc_other'
     inc_id = Column(INTEGER(), primary_key=True, autoincrement=True,
                     mssql_identity_start=1000, mssql_identity_increment=1)
@@ -138,7 +134,7 @@ class input_inc_other(Base):
     set_id = Column(INTEGER(), ForeignKey("f_set.set_id"), nullable=False)
 
 
-class input_inc_other_p(Base):
+class inc_other_p(Base):
     __tablename__ = "input_inc_other_p"
     inc_id = Column(INTEGER(), ForeignKey(
         "input_inc_other.inc_id"), primary_key=True, nullable=False)
@@ -146,7 +142,7 @@ class input_inc_other_p(Base):
     amount = Column(FDec, nullable=True)
 
 
-class input_inc_bursary(Base):
+class inc_bursary(Base):
     __tablename__ = 'input_inc_bursary'
 
     set_id = Column(INTEGER(), ForeignKey("f_set.set_id"),
@@ -157,7 +153,15 @@ class input_inc_bursary(Base):
     status = Column(CHAR(1), nullable=True)
 
 
-class input_pay_fracclaim(Base):
+class inc_feeloss(Base):
+    __tablename__ = 'input_inc_feeloss'
+
+    set_id = Column(INTEGER(), ForeignKey("f_set.set_id"), primary_key=True)
+    status = Column(CHAR(1), primary_key=True)
+    rate = Column(DECIMAL(precision=10, scale=5), nullable=True)
+
+
+class pay_fracclaim(Base):
     __tablename__ = 'input_pay_fracclaim'
 
     period = Column(INTEGER(), nullable=True, primary_key=True)
@@ -166,15 +170,40 @@ class input_pay_fracclaim(Base):
                     primary_key=True,  nullable=False)
 
 
-class input_inc_feeloss(Base):
-    __tablename__ = 'input_inc_feeloss'
+class claim_type(Base):
+    __tablename__ = "input_pay_claim_type"
 
-    set_id = Column(INTEGER(), ForeignKey("f_set.set_id"), primary_key=True)
-    status = Column(CHAR(1), primary_key=True)
-    rate = Column(DECIMAL(precision=10, scale=5), nullable=True)
+    claim_type_id = Column(CHAR(3), primary_key=True)
+    description = Column(VARCHAR(50))
 
 
-class input_pay_staff(Base):
+class pay_claim(Base):
+    __tablename__ = "input_pay_claim"
+
+    claim_id = Column(INTEGER(), primary_key=True, autoincrement=True,
+                      mssql_identity_start=1000, mssql_identity_increment=1)
+    set_id = Column(INTEGER(), ForeignKey("f_set.set_id"))
+    account = Column(CHAR(4), ForeignKey("fs_account.account"))
+    description = Column(VARCHAR(50))
+    rate = Column(FDec)
+    claim_type_id = Column(CHAR(3), ForeignKey(
+        "input_pay_claim_type.claim_type_id"))
+
+    hours = relationship("input_pay_claim_p", back_populates="claim")
+
+
+class pay_claim_p(Base):
+    __tablename__ = "input_pay_claim_p"
+
+    claim_id = Column(INTEGER(), ForeignKey(
+        "input_pay_claim.claim_id"), primary_key=True)
+    period = Column(INTEGER(), primary_key=True)
+    hours = Column(DECIMAL(10, 5))
+
+    claim = relationship("input_pay_claim", back_populates="hours")
+
+
+class pay_staff(Base):
     __tablename__ = "input_pay_staff"
 
     post_status_id = Column(CHAR(4), ForeignKey(
@@ -242,16 +271,16 @@ class pension(Base):
     emp_cont_rate = Column(DECIMAL(10, 8), nullable=False)
 
 
-class input_nonp_other(Base):
-    __tablename__ = 'input_non_other'
+class nonp_other(Base):
+    __tablename__ = 'input_nonp_other'
     nonp_id = Column(INTEGER(), primary_key=True, autoincrement=True,
                      mssql_identity_start=1000, mssql_identity_increment=1)
-    account = Column(CHAR(4), nullable=True)
+    account = Column(CHAR(4), ForeignKey("fs_account.account"), nullable=True)
     description = Column(VARCHAR(250), nullable=True)
     set_id = Column(INTEGER(), ForeignKey("f_set.set_id"), nullable=False)
 
 
-class input_nonp_other_p(Base):
+class nonp_other_p(Base):
     __tablename__ = "input_nonp_other_p"
     nonp_id = Column(INTEGER(), ForeignKey(
         "input_nonp_other.nonp_id"), primary_key=True, nullable=False)
@@ -259,13 +288,13 @@ class input_nonp_other_p(Base):
     amount = Column(FDec, nullable=True)
 
 
-class input_nonp_internal(Base):
+class nonp_internal(Base):
     __tablename__ = "input_nonp_internal"
 
     internal_id = Column(INTEGER(), primary_key=True, autoincrement=True,
                          mssql_identity_start=1000, mssql_identity_increment=1)
     description = Column(VARCHAR(50), nullable=True)
-    costc = Column(CHAR(6), ForeignKey("cost_centre.costc"), nullable=True)
+    costc = Column(CHAR(6), ForeignKey("fs_cost_centre.costc"), nullable=True)
     amount = Column(FDec, nullable=True)
 
 
