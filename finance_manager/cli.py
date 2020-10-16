fd90e910118d74cd6d3d8af88cc9a90d57f52d17
James Boyes <james.boyes@lcm.ac.uk>
2020-10-16 15:48:39 +0100

    Added load from csv

--- finance_manager/cli.py
+++ finance_manager/cli.py
@@ -1,6 +1,10 @@
 import click
+import progressbar
+import csv
+import os
+import sys
 
-from finance_manager.database.spec import f_set, f_set_cat
+from finance_manager.database.spec import table_map, f_set, f_set_cat
 from finance_manager.database.db import DB
 from finance_manager.config import Config
 
@@ -16,6 +20,49 @@ def fm(config):
     This application was designed to be a single point of reference for administration tasks.
     """
     pass
+
+
+@fm.command()
+@click.option("--overwrite", is_flag=True, help="Clear table first")
+@click.argument("table_name")
+@click.argument("filepath")
+@pass_config
+def load(config, overwrite, table_name, filepath):
+    """
+    Load records into a table 
+    """
+    if table_name not in table_map.keys():
+        raise ValueError("Invalid table")
+
+    if not os.path.isfile(filepath):
+        raise ValueError("Invalid filepath")
+    table = table_map[table_name]
+    with DB(config=config) as db:
+        session = db.session()
+        try:
+            if overwrite:
+                click.echo("Deleting existing records")
+                session.query(table).delete()
+            with open(filepath, newline='') as f:
+                rdr = csv.reader(f)
+                # Load into memory
+                rows = [row for row in rdr]
+                click.echo(f"{len(rows)} rows found in file")
+                first_row = True
+                for i in progressbar.progressbar(range(len(rows))):
+                    if first_row:
+                        labels = [val for val in rows[i]]
+                        first_row = False
+                    else:
+                        values = {x[0]: x[1] for x in zip(labels, rows[i])}
+                        record = table(**values)
+                        session.add(record)
+            click.echo("Committing")
+            session.commit()
+            click.echo("Success")
+        except Exception as inst:
+            session.rollback()
+            click.echo(inst)
 
 
 @fm.group()
@@ -37,18 +84,23 @@ def codes(config):
 
 
 @codes.command()
+@click.option('--overwrite', is_flag=True, help="Overwrite existing value if exists")
 @click.argument('code')
 @click.argument('description')
 @pass_config
-def add(config, code, description):
+def add(config, code, description, overwrite):
     """
-    Add a new set code
+    Add a new set category
     """
     new_code = f_set_cat(set_cat_id=code, description=description)
-    with DB(config=config, debug=True) as db:
+    with DB(config=config) as db:
         session = db.session()
-        session.add(new_code)
-        session.commit()
+        try:
+            session.add(new_code)
+            session.commit()
+        except:
+            if overwrite:
+                session.query(f_set)
 
 
 @fm.group()
