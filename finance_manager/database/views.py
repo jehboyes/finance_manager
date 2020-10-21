defe52544171f7bfe165471ccd89411166027fad
James Boyes <james.boyes@lcm.ac.uk>
2020-10-21 16:58:18 +0100

    Imporved views

--- finance_manager/database/views.py
+++ finance_manager/database/views.py
@@ -2,29 +2,38 @@ from finance_manager.database.replaceable import ReplaceableObject as o
 from finance_manager.functions import periods
 p = [f'p{n}' for n in periods()]
 p_sum_string = "+".join(p)
+p_list_string = ", ".join(p)
+
+account_description = "a.account + ' ' + a.description as account_description"
 
 views = [o("v_input_inc_other", f"""
-            SELECT i.account, a.description as account_name,i.description, i.set_id, 
-            SUM({p_sum_string}) as amount
+            SELECT i.inc_id, i.account, a.description as account_name, {account_description}, i.description, i.set_id,
+            {p_list_string}, {p_sum_string} as amount
             FROM input_inc_other i
-            INNER JOIN fs_account a ON i.account = a.account
-            GROUP BY i.inc_id, i.account, a.description, i.description, i.set_id"""),
+            LEFT OUTER JOIN fs_account a ON i.account = a.account
+            """),
 
          o("v_input_nonp_other", f"""
-            SELECT i.account, a.description as account_name, i.description, i.set_id, 
-            SUM({p_sum_string}) as amount
+            SELECT i.nonp_id, i.account, a.description as account_name, {account_description}, i.description, i.set_id,
+            {p_list_string}, {p_sum_string} as amount
             FROM input_nonp_other i
-            INNER JOIN fs_account a ON i.account = a.account
-            GROUP BY i.nonp_id, i.account, a.description, i.description, i.set_id"""),
+            LEFT OUTER JOIN fs_account a ON i.account = a.account
+            """),
 
          o("v_input_pay_claim", f"""
-            SELECT i.set_id, i.claim_id, i.account, i.description, i.rate, t.description as claim_type, 
+            SELECT i.set_id, i.claim_id, i.account, i.description, {account_description}, i.rate, t.description as claim_type,
 	        a.description as account_name,
-	        SUM({p_sum_string}) as hours, SUM(({p_sum_string})*i.rate) as amount
-            FROM input_pay_claim i 
-            INNER JOIN input_pay_claim_type t ON i.claim_type_id = t.claim_type_id
-            INNER JOIN fs_account a ON i.account = a.account
-            GROUP BY i.set_id, i.claim_id, i.account, i.description, i.rate, t.description, 
-                a.description""")
+	        {p_list_string}, {p_sum_string} as hours, ({p_sum_string})*i.rate as amount
+            FROM input_pay_claim i
+            LEFT OUTER JOIN input_pay_claim_type t ON i.claim_type_id = t.claim_type_id
+            LEFT OUTER JOIN fs_account a ON i.account = a.account
+            """),
+         o("v_fs_account", f"""
+            SELECT a.account, a.description, {account_description}, 
+            s.description as summary_description, s.section_id
+            FROM fs_account a
+            INNER JOIN fs_summary_code s ON s.summary_code = a.summary_code 
+            WHERE a.hide_from_users = 0
+         """)
 
          ]
