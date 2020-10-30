10d5d79a0d7216fd863f8d1287018b5584ebc374
James Boyes <james.boyes@lcm.ac.uk>
2020-10-30 14:40:57 +0000

    Basic permission granting

--- finance_manager/cli/permissions/__init__.py
+++ finance_manager/cli/permissions/__init__.py
@@ -1,10 +1,16 @@
 import click
+from .add import add as a
+from .director import director as d
 
 
 @click.group()
 @click.pass_obj
-def permissions():
+def permissions(config):
     """
     Manage access to cost centres in the UI.
     """
     pass
+
+
+permissions.add_command(a)
+permissions.add_command(d)
