0660f59a3904e1cf31aee8dc82a6c7fc903f38ea
James Boyes <james.boyes@lcm.ac.uk>
2020-11-02 15:48:40 +0000

    Fix typo

--- finance_manager/database/views/__init__.py
+++ finance_manager/database/views/__init__.py
@@ -176,7 +176,7 @@ SELECT m.*,
 FROM v_calc_staff_monthly m
 INNER JOIN v_calc_staff_monthly_total mt ON m.staff_Id = mt.staff_id 
                                          AND m.acad_year = mt.acad_year 
-                                         AND m.set_cat_id = m.set_cat_id
+                                         AND m.set_cat_id = mt.set_cat_id
 INNER JOIN input_pay_staff s ON s.staff_line_id = m.staff_line_id
 LEFT OUTER JOIN staff_pension_contrib pension ON pension.pension_id = s.pension_id AND pension.acad_year = m.acad_year
 INNER JOIN staff_ni ni ON ni.acad_year = m.acad_year 
