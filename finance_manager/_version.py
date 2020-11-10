54a8a3a52866de61769c45f4b9f419a800a905db
jehboyes <jehboyes@gmail.com>
2020-11-10 15:00:30 +0000

    Still trying to fix docs

--- finance_manager/_version.py
+++ finance_manager/_version.py
@@ -1,11 +1,1 @@
-"""
-Provides finance_manager version information.
-"""
-
-# This file is auto-generated! Do not edit!
-# Use `python -m incremental.update finance_manager` to change this file.
-
-from incremental import Version
-
-__version__ = Version('finance_manager', 1, 0, 1, dev=0)
-__all__ = ["__version__"]
+__version__ = "1.0.1"
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-from setuptools import setup
+import setuptools
+version = {}
+with open("finance_manager/_version.py") as fp:
+    exec(fp.read(), version)
 
-setup(
+
+# later on we use: version['__version__']
+setuptools.setup(
     name='finance_manager',
-    version='0.1',
+    version=version['__version__'],
     py_modules=['finance_manager'],
-    use_incremental=True,
-    setup_requires=['incremental'],
     install_requires=[
-        'incremental',
         'Click',
     ],
     entry_points='''
         [console_scripts]
         fm=finance_manager.cli:fm
     ''',
+    packages=setuptools.find_packages()
 )
