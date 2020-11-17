73b6b4ba81de2d5e0bdb7b49d6ee2d6b8af4e013
James Boyes <james.boyes@lcm.ac.uk>
2020-11-17 12:53:58 +0000

    Added ability to handle new set_code_id

--- finance_manager/cli/database/newset.py
+++ finance_manager/cli/database/newset.py
@@ -1,24 +1,25 @@
 # pylint: disable=no-member
 import click
-
 from finance_manager.database import DB
-from finance_manager.database.spec import f_set, cost_centre
+from finance_manager.database.spec import f_set, cost_centre, f_set_cat
 
 
 @click.command()
 @click.option("--change_sn", help="Allow users to change student numbers", is_flag=True)
+@click.option("--new", help="Create a new set category with the name given to this option", type=str)
 @click.argument("setcode")
 @click.argument("acad_year", type=int)
 @click.argument("curriculum")
 @click.argument("sn_usage")
 @click.pass_obj
-def newset(config, acad_year, setcode, curriculum, sn_usage, change_sn):
+def newset(config, acad_year, setcode, curriculum, sn_usage, change_sn, new=None):
     """
     Create new finance sets.
 
     Create a new set for each un-superceded cost centre, in ``ACAD_YEAR`` 
     with ``SETCODE``, using the id of the ``CURRICULUM``, and student number usage from 
-    SN_USAGE. 
+    SN_USAGE. If the ``new`` option is used, a new set caytegory will be created 
+    with the set cat ID given. 
     """
     with DB(config=config) as db:
         s = db.session()
@@ -30,12 +31,20 @@ def newset(config, acad_year, setcode, curriculum, sn_usage, change_sn):
             change_sn = 1
         else:
             change_sn = 0
-        for costc in cost_centres:
-            # Create a new set with given detail
-            s.add(f_set(acad_year=acad_year,
-                        set_cat_id=setcode,
-                        curriculum_id=curriculum,
-                        student_number_usage_id=sn_usage,
-                        allow_student_number_change=change_sn,
-                        costc=costc.costc))
-        s.commit()
+        if new != None:  # Create the new set cat ID
+            s.add(f_set_cat(set_cat_id=setcode,
+                            description=new))
+            s.flush()  # so that the set can be added
+        try:
+            for costc in cost_centres:
+                # Create a new set with given detail
+                s.add(f_set(acad_year=acad_year,
+                            set_cat_id=setcode,
+                            curriculum_id=curriculum,
+                            student_number_usage_id=sn_usage,
+                            allow_student_number_change=change_sn,
+                            costc=costc.costc))
+            s.commit()
+        except:
+            raise ValueError(
+                "Foreign key violated - check the set_cat_id is valid, or use the --new option to create a new one")
