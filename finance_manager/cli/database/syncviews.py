49d08aeadce6efef2abdfb0e2e9e9dbdb7c4c2fd
James Boyes <james.boyes@lcm.ac.uk>
2020-11-17 12:54:18 +0000

    Added dependency ordering

--- finance_manager/cli/database/syncviews.py
+++ finance_manager/cli/database/syncviews.py
@@ -1,11 +1,13 @@
 # pylint: disable=no-member
 import click
 from finance_manager.database.views import get_views
+from finance_manager.config import Config as conf
 from finance_manager.database import DB
 from datetime import datetime
 from getpass import getuser
-import pkg_resources  # part of setuptools
-version = pkg_resources.require("finance_manager")[0].version
+import warnings
+import finance_manager._version as ver
+version = ver.__version__
 
 stamp = f"""
 -- ===================================================
@@ -31,19 +33,43 @@ def syncviews(config, test, restrict):
 
     Pushes view definitions from this application's database views.  
     """
-    view_list = get_views()
     with DB(config=config) as db:
         views = get_views()
-        with click.progressbar(views, label="Updating views") as bar:
+        ordering = [n for n in range(len(views))]
+        # Establish dependency
+        swap_occurred = True
+        i = 0
+        lim = 100000
+        click.echo(
+            "Determining view dependencies (for ordering CREATE execution)")
+        while swap_occurred and i < lim:
+            swap_occurred = False
+            views = [views[i] for i in ordering]
+            ordering = [n for n in range(len(views))]
+            for pos_v, v in enumerate(views):
+                for pos_vc, vc in enumerate(views):
+                    if v.name in vc.sqltext and pos_v > pos_vc:
+                        # swap them in the execution order
+                        ordering[pos_v] = pos_vc
+                        ordering[pos_vc] = pos_v
+                        swap_occurred = True
+                        i += 1
+                        break
+                if swap_occurred:
+                    break
+        if i >= lim:
+            warnings.warn(
+                f"Ordering **unfinished** after {lim} attempts. Indicative of circular reference in views.", RuntimeWarning)
+        views = [views[i] for i in ordering]
+        pb_label = "Updating views"
+        if test:
+            pb_label += " and testing in database"
+        with click.progressbar(views, label=pb_label) as bar:
             for v in bar:
                 if restrict is None or v.name == restrict:
                     sql = f"\nDROP VIEW IF EXISTS {v.name}"
                     db.con.execute(sql)
                     sql = f"CREATE VIEW {v.name} AS {stamp}\n{v.sqltext}"
                     db.con.execute(sql)
-        if test:
-            with click.progressbar(views, label="Testing views") as bar:
-                for v in bar:
-                    if restrict is None or v.name == restrict:
-                        _ = db.con.execute(
-                            "SELECT * FROM " + v.name).fetchall()
+                    if test:
+                        _ = db.con.execute(f"SELECT * FROM {v.name}").fetchall()
