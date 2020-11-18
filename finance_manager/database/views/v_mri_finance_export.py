c51be520c6a8999751d3af73422a80d3f646448c
James Boyes <james.boyes@lcm.ac.uk>
2020-11-18 22:14:30 +0000

    Round to 2 dp

--- finance_manager/database/views/v_mri_finance_export.py
+++ finance_manager/database/views/v_mri_finance_export.py
@@ -4,7 +4,7 @@ from finance_manager.database.replaceable import ReplaceableObject as o
 def _view():
     sql = """
 	SELECT s.set_cat_id, f.acad_year, f.costc, ISNULL(m.t, f.account) as account, f.period, 
-        SUM(f.amount*f.coefficient) as amount, CASE f.costc WHEN 'MZ1000' THEN 1 ELSE 0 END as governors
+        ROUND(SUM(f.amount*f.coefficient),2) as amount, CASE f.costc WHEN 'MZ1000' THEN 1 ELSE 0 END as governors
     FROM v_mri_finance f  
     INNER JOIN f_set s ON s.set_id = f.set_id
     LEFT JOIN (SELECT * FROM (VALUES (1900, 1240), (1901, 1245)) x(f, t)) as m ON m.f=f.account
