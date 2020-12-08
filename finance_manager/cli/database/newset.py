073ffd1f70cfdaa97d344f0e9a02bc8c1b5f8830
James Boyes <james.boyes@lcm.ac.uk>
2020-12-08 09:55:42 +0000

    typo in docstring

--- finance_manager/cli/database/newset.py
+++ finance_manager/cli/database/newset.py
@@ -18,7 +18,7 @@ def newset(config, acad_year, setcode, curriculum, sn_usage, change_sn, new=None
 
     Create a new set for each un-superceded cost centre, in ``ACAD_YEAR`` 
     with ``SETCODE``, using the id of the ``CURRICULUM``, and student number usage from 
-    SN_USAGE. If the ``new`` option is used, a new set caytegory will be created 
+    SN_USAGE. If the ``new`` option is used, a new set category will be created 
     with the set cat ID given. 
     """
     with DB(config=config) as db:
