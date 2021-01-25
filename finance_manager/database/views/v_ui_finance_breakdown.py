73853b5bb0b9431bea4b73c0bf9ee8f7e72dc0fd
James Boyes <james.boyes@lcm.ac.uk>
2021-01-25 12:56:17 +0000

    Added hidden accounts back into the breakdown.

--- finance_manager/database/views/v_ui_finance_breakdown.py
+++ finance_manager/database/views/v_ui_finance_breakdown.py
@@ -4,7 +4,6 @@ from finance_manager.database.replaceable import ReplaceableObject as o
 def _view():
     view = o("v_ui_finance_breakdown", f"""
     SELECT set_id, summary_code, account, account_description, sum(amount) as amount FROM v_mri_finance
-    WHERE hide_from_users <> 1
     GROUP BY set_id, summary_code, account, account_description
     """)
     return view
