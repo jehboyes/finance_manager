db6e3e9af860af66b69c008e5a722eb7704e10eb
James Boyes <james.boyes@lcm.ac.uk>
2021-01-28 17:12:46 +0000

    Performance improvements to finance save
    Previous version called computationally-expensive
    function for each cost centre,
    now more efficient and makes better
    use of ORM features.

--- finance_manager/cli/finance/save.py
+++ finance_manager/cli/finance/save.py
@@ -6,6 +6,7 @@ from getpass import getuser
 from finance_manager.database import DB
 from finance_manager.database.spec import f_set, finance, finance_instance
 from finance_manager.database.views.v_calc_finances import _view
+from collections import defaultdict
 
 
 @click.command()
@@ -21,20 +22,43 @@ def save(config, acad_year, setcat, restrict=None):
     """
     with DB(config=config) as db:
         session = db.session()
+
+        # Get sets to be updated
         sets = session.query(f_set).filter(and_(f_set.acad_year == acad_year,
                                                 f_set.set_cat_id == setcat))
         if restrict != None:  # add costc filter if passed
             click.echo(f"Restricting to {restrict}")
             sets = sets.filter(f_set.costc == restrict)
         sets = sets.all()
-        with click.progressbar(sets, label="Working through sets") as bar:
+
+        # Calculate the actual finances
+        click.echo("Calculating finances...", nl=False)
+        calc_finances = session.execute(
+            f"SELECT account, period, amount, set_id FROM {_view().name}")
+        click.echo("Complete.")
+
+        # COnvert the results to a dictionary by set_id for easier processing
+        dict_finances = defaultdict(list)
+        for r in calc_finances:
+            dict_finances[r[3]].append(r)
+
+        # For each set (wrapped for progress bar)
+        with click.progressbar(sets, label="Working through sets", show_eta=False, item_show_func=_progress_label, fill_char="£") as bar:
             for s in bar:
                 # Make it a finance set
                 i = finance_instance(created_by=getuser(),
                                      set_id=s.set_id, datestamp=datetime.now())
                 session.add(i)
                 session.flush()
-                sql = f"""INSERT INTO {finance.__tablename__} (instance_id, account, period, amount) 
-                        SELECT {i.instance_id}, account, period, amount FROM {_view().name} WHERE set_id = {s.set_id}"""
-                session.execute(sql)
+                # create a list of finance objects for buk inserting, way quicker than one by one
+                finances = []
+                for row in dict_finances[s.set_id]:
+                    finances.append(finance(instance_id=i.instance_id,
+                                            account=row[0], period=row[1], amount=row[2]))
+                session.bulk_save_objects(finances)
                 session.commit()
+
+
+def _progress_label(s):
+    if s is not None:
+        return f"Processing {s.costc}"
