a806dcb37f541a4a890956f9e49097432f8727ae
James Boyes <james.boyes@lcm.ac.uk>
2021-01-28 17:08:06 +0000

    Improve staff list view
    Added extra fields for better interaction with app.
    Specifically, descriptions for each of the foregin key fields.

--- finance_manager/database/views/v_input_pay_staff.py
+++ finance_manager/database/views/v_input_pay_staff.py
@@ -9,11 +9,24 @@ staff_total_pension = "+".join([f"a.pension_p{n}" for n in periods()])
 
 sql = f"""
 SELECT s.*, 
+    sps.description as post_status_desc, 
+    sps.colour_hex as hex,
+    spt.description as post_type_desc,
+    sp.description as pension_desc, 
+    sct.description as con_desc, 
 {staff_total_pay} as pay_total,
 {staff_total_ni} as ni_total,
-{staff_total_pension} as pension_total
+{staff_total_pension} as pension_total, 
+    {staff_total_pay} + {staff_total_ni} + {staff_total_pension} + ISNULL(travel_scheme,0) as total 
 FROM input_pay_staff AS s
 LEFT OUTER JOIN v_calc_staff_monthly_all a ON a.staff_line_id = s.staff_line_id
+LEFT OUTER JOIN staff_post_status sps ON sps.post_status_id = s.post_status_id
+LEFT OUTER JOIN staff_post_type spt ON spt.post_type_id = s.post_type_id 
+LEFT OUTER JOIN staff_pension sp ON sp.pension_id = s.pension_id
+INNER JOIN f_set f ON f.set_id = s.set_id
+LEFT OUTER JOIN staff_con_type sct ON sct.con_type_id = s.con_type_id 
+                                    AND sct.acad_year = f.acad_year
+                                    AND sct.set_cat_id = f.set_cat_id
 """
 
 
