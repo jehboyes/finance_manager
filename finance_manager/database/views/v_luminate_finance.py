cfdd50a76314a0ec1b31593249998543f76f170c
James Boyes <james.boyes@lcm.ac.uk>
2021-02-22 11:13:03 +0000

    Add consolidation to view
    Uses a cross join to produce a version with the
    directorate_id given as Z.

--- finance_manager/database/views/v_luminate_finance.py
+++ finance_manager/database/views/v_luminate_finance.py
@@ -3,10 +3,14 @@ from finance_manager.database.replaceable import ReplaceableObject as o
 
 
 sql = f"""
-SELECT v.*, s.acad_year, s.costc, CAST(s.acad_year as CHAR(4)) + ' ' + s.set_cat_id as finance_summary, c.directorate_id 
+SELECT v.*, s.acad_year, s.costc, CAST(s.acad_year as CHAR(4)) + ' ' + s.set_cat_id as finance_summary, 
+    CASE x.n WHEN 1 THEN c.directorate_id ELSE 'Z' END as directorate_id, 
+    s.costc + ' ' + c.description as costc_description
 FROM v_mri_finance_grouped_subtotal v
 INNER JOIN f_set s ON v.set_id = s.set_id 
 INNER JOIN fs_cost_centre c on c.costc = s.costc
+CROSS JOIN (SELECT * FROM (VALUES (1), (2)) x(n)) as x 
+WHERE c.costc <> 'MZ1000'
 """
 
 
