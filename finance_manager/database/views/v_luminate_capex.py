2e6171009c2587cc76b676f9d030a6c1ab1697d8
James Boyes <james.boyes@lcm.ac.uk>
2021-03-03 12:43:28 +0000

    Remove 0-value capital from luminate view

--- finance_manager/database/views/v_luminate_capex.py
+++ finance_manager/database/views/v_luminate_capex.py
@@ -3,11 +3,11 @@ from finance_manager.database.replaceable import ReplaceableObject as o
 
 
 sql = f"""
-SELECT v.*, f.set_cat_id, f.acad_year, cc.directorate_id 
+SELECT v.*, f.set_cat_id, f.acad_year, 'E' as directorate_id 
 FROM v_input_capex v
 INNER JOIN f_set f ON f.set_id = v.set_id 
 INNER JOIN fs_cost_centre cc ON cc.costc = f.costc 
-WHERE f.surpress = 0
+WHERE f.surpress = 0 AND v.total_amount > 0
 """
 
 
