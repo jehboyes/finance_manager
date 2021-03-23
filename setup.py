829cd66795c57a513eb070ee833166a9f7a2f4d3
James Boyes <james.boyes@lcm.ac.uk>
2021-03-23 13:30:31 +0000

    Added metadata to setup

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ version = {}
 with open("finance_manager/_version.py") as fp:
     exec(fp.read(), version)
 
+with open("README.md", "r") as f:
+    long_description = f.read()
 
-# later on we use: version['__version__']
 setuptools.setup(
     name='finance_manager',
     version=version['__version__'],
@@ -12,9 +13,14 @@ setuptools.setup(
     install_requires=[
         'Click',
     ],
+    author="James Boyes",
+    author_email="James.Boyes@lcm.ac.uk",
+    description="Managing HE Financial Forecasting and Planning",
     entry_points='''
         [console_scripts]
         fm=finance_manager.cli:fm
     ''',
+    long_description=long_description,
+    url="https://github.com/jehboyes/finance_manager",
     packages=setuptools.find_packages()
 )
