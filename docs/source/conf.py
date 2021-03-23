65de964cf64d87c498b2f176de13472d172c0916
James Boyes <james.boyes@lcm.ac.uk>
2021-03-23 11:46:13 +0000

    Updated copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Finance Manager'
-copyright = '2020, James E H Boyes'
+copyright = '2020-2021, James E H Boyes'
 author = 'James E H Boyes'
 
 # The full version, including alpha/beta/rc tags
