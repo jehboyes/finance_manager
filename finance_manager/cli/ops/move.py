45f7ca0fb23db41c2241bdf7dacfbf3dd2a093d6
James Boyes <james.boyes@lcm.ac.uk>
2021-04-13 17:09:09 +0100

    Added replace option to move command
    Allows users to replace the pre-existing
    contents in the selected table for the target set.

--- finance_manager/cli/ops/move.py
+++ finance_manager/cli/ops/move.py
@@ -17,8 +17,9 @@ lookup = {"staff": [pay_staff, "staff_line_id"],
 @click.argument("set_cat_id", type=str)
 @click.argument("obj", type=click.Choice([k for k in lookup.keys()]))
 @click.argument("cmd", type=click.Choice(["copy", "move"]))
+@click.option("--replace", "-r", is_flag=True, help="Replace the current contents of the target set.")
 @click.pass_obj
-def move(config, from_costc, to_costc, acad_year, set_cat_id, obj, cmd):
+def move(config, from_costc, to_costc, acad_year, set_cat_id, obj, cmd, replace):
     """
     Move or copy instances between input tables. 
 
@@ -42,14 +43,22 @@ def move(config, from_costc, to_costc, acad_year, set_cat_id, obj, cmd):
         target_set = s.query(f_set).filter(and_(f_set.costc == to_costc,
                                                 f_set.acad_year == acad_year,
                                                 f_set.set_cat_id == set_cat_id)).first()
+        replace_txt = ""
+        if replace:
+            target_preexist = s.query(db_obj).filter(
+                db_obj.set_id == target_set.set_id)
+            preexist_n = len(target_preexist.all())
+            target_preexist.delete()
+            replace_txt = f"(replacing {preexist_n} pre-existing rows) "
+        # list of fields that will change, which is the PK (from the lookup) and the set_id
         change_fields = [lookup[obj][1], 'set_id']
         i = 0
         for old_line in lines:
             new_line = db_obj()
             i += 1
+            # for each column, set the new val = old val if its not one of the change_fields
             for ax in old_line.__table__.columns:
                 a = str(ax).split(".")[1]
-
                 if a not in change_fields:
                     setattr(new_line, a, getattr(old_line, a))
             new_line.set_id = target_set.set_id
@@ -57,6 +66,7 @@ def move(config, from_costc, to_costc, acad_year, set_cat_id, obj, cmd):
                 s.delete(old_line)
             s.add(new_line)
             s.flush()
-        if click.confirm(f"Confirm {cmd} {i} {obj} lines from {from_costc} to {to_costc} in {acad_year} {set_cat_id}?"):
+
+        if click.confirm(f"Confirm {cmd} {i} {obj} lines {replace_txt} from {from_costc} to {to_costc} in {acad_year} {set_cat_id}?"):
             s.commit()
         s.rollback()
